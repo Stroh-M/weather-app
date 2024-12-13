249243ba4b5583dc6375643b72f06de57c5ff877
Meir Stroh <meirstroh6@gmail.com>
2024-12-13 15:09:14 -0500

    add crypto currency info and prices endpoint

--- server/server.js
+++ server/server.js
@@ -7,6 +7,9 @@ dotenv.config();
 const app = express();
 const port = 5000;
 
+// ROUTES //
+
+// CURRENT WEATHER WITH SPECIFIC LOCATION ///////////////////////////////////////////////////////////////////////////////////
 app.get("/weather/current/:location", async (req, res) => {
   try {
     const { location } = req.params;
@@ -20,6 +23,7 @@ app.get("/weather/current/:location", async (req, res) => {
   }
 });
 
+// FORECAST FOR LOCATION SPECIFIED ///////////////////////////////////////////////////////////////////////////////////////////
 app.get("/weather/forecast/:location", async (req, res) => {
   try {
     const { location } = req.params;
@@ -27,6 +31,35 @@ app.get("/weather/forecast/:location", async (req, res) => {
       `http://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_API_KEY}&q=${location}&days=3&api=no&alerts=no`
     );
     res.json(result.data.forecast);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// CRYPTO PRICES ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+app.get("/crypto/prices", async (req, res) => {
+  try {
+    // getting crypto prices
+    const result = await axios.get(
+      `http://api.coinlayer.com/api/live?access_key=${process.env.CRYPTO_CURRENCY_API_KEY}&symbols=BTC,ETH,USDT,XRP,SOL`
+    );
+
+    console.log(result.data);
+    res.json(result.data);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// CRYPTO LIST/INFO //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+app.get("/crypto/info", async (req, res) => {
+  try {
+    // list of crypto currencies info e.g. name, full_name, icons, symbol
+    const result = await axios.get(
+      `http://api.coinlayer.com/list?access_key=${process.env.CRYPTO_CURRENCY_API_KEY}`
+    );
+    console.log(result.data);
+    res.json(result.data);
   } catch (err) {
     console.error(err.message);
   }
